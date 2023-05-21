852a3698a048ee863cef8e83d21b7b15c47aadbf
xotic <xoticc.ja@gmail.com>
2023-05-21 11:10:49 -0400

    Implemented complete button

--- gui.py
+++ gui.py
@@ -9,40 +9,59 @@ list_box = gui.Listbox(values=[todo.strip() for todo in get_todos()], key='todos
 add_button = gui.Button("Add")
 edit_button = gui.Button("Edit")
 complete_button = gui.Button("Complete")
+exit_button = gui.Button("Exit")
 
 window = gui.Window('To-Do App', 
-                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                    layout=[[label],
+                            [input_box, add_button],
+                            [list_box, edit_button, complete_button],
+                            [exit_button]],
                     font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
 
+    print(event)
+    print(values)
     if event == "Add":
         todos = get_todos()
         new_todo = values['todo']
-        todos.append(new_todo + '\n')
+        todos.append(new_todo.strip() + '\n')
         write_todos(todos)
 
         input_box.update(value="")
         list_box.update(values=[todo.strip() for todo in todos])
 
     elif event == "Edit":
-        todo_to_edit = values['todos'][0]
-        new_todo = values['todo']
+        try:
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
 
-        todos = get_todos()
-        index = todos.index(todo_to_edit  + '\n')
-        todos[index] = new_todo + '\n'
-        write_todos(todos)
+            todos = get_todos()
+            index = todos.index(todo_to_edit.strip() + '\n')
+            todos[index] = new_todo.strip() + '\n'
+            write_todos(todos)
 
-        input_box.update(value="")
-        list_box.update(values=[todo.strip() for todo in todos])
+            input_box.update(value="")
+            list_box.update(values=[todo.strip() for todo in todos])
+        except (IndexError, ValueError):
+            print("Error editing todo.")
 
     elif event == 'todos':
-        window['todo'].update(value=values['todos'][0])
-
+        try:
+            window['todo'].update(value=values['todos'][0])
+        except IndexError:
+            print("Index Error")
+            
     elif event == "Complete":
-        pass
+        try:
+            todo_to_complete = values['todos'][0]
+            todos = get_todos()
+            todos.remove(todo_to_complete + '\n')
+            write_todos(todos)
+            list_box.update(values=[todo.strip() for todo in todos])
+        except IndexError:
+            print("Index Error")
 
     elif event == "Exit" or event == gui.WINDOW_CLOSED:
         break
